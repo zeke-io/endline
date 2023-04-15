7120f3b34a7926745248263aa212e747ec463dae
zeke-io <ezequiel_hvm@protonmail.com>
2023-04-15 13:49:57 -0300

    Add dev command

--- src/bin/endline.ts
+++ src/bin/endline.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-/**
- * Behold, the first line of code.
- * Either this project is going to thrive, or fail and fall into oblivion.
- * Regardless, this is the start of a new journey.
- * Why am I writing this anyway?
- */
-console.log('Hello, World!')
+import { Command } from 'commander'
+import EndlineDev from '../cli/endline-dev'
+
+const program = new Command()
+
+program.name('endline')
+program.addCommand(EndlineDev.command, { isDefault: true })
+
+program.parse()
