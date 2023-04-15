ab7ca113243e6aa12e14608bc40cdbd8ddb7ffb3
zeke-io <ezequiel_hvm@protonmail.com>
2023-04-15 11:17:53 -0300

    Add endline binary

---
+++ src/bin/endline.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+/**
+ * Behold, the first line of code.
+ * Either this project is going to thrive, or fail and fall into oblivion.
+ * Regardless, this is the start of a new journey.
+ * Why am I writing this anyway?
+ */
+console.log('Hello, World!')
