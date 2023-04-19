e294ececd74ea55c2e0d9f64460d69f13dd8b169
zeke-io <ezequiel_io@pm.me>
2023-04-18 22:42:15 -0300

    Add "with .env files" example

---
+++ examples/with-env-files/src/api/index.js
@@ -0,0 +1,16 @@
+const GITHUB_REPOSITORY_URL = process.env.GITHUB_REPOSITORY_URL
+const NPM_PACKAGE_URL = process.env.NPM_PACKAGE_URL
+
+async function IndexRoute(router) {
+  router.GET('/', async ({ res }) => {
+    const payload = {
+      repositoryUrl: GITHUB_REPOSITORY_URL,
+      npmPackageUrl: NPM_PACKAGE_URL,
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(payload))
+  })
+}
+
+module.exports = IndexRoute
