ebac4efd901e32b8d9042ab7839e1619d1aca91c
zeke-io <ezequiel_io@pm.me>
2023-04-18 23:03:07 -0300

    Add "custom paths" example

---
+++ examples/custom-paths/endline.config.js
@@ -0,0 +1,11 @@
+/**
+ * By default, Endline looks for route files in the `src/api` directory,
+ * this can be overridden in this configuration file.
+ */
+const config = {
+  router: {
+    apiFolderPath: 'src/routes',
+  },
+}
+
+module.exports = config
