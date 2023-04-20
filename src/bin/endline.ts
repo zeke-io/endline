38fb25b844e322e1bea7f6802b68904e19a2493b
zeke-io <ezequiel_io@pm.me>
2023-04-19 21:16:19 -0300

    Add cli index

--- src/bin/endline.ts
+++ src/bin/endline.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
-import EndlineDev from '../cli/endline-dev'
+import { EndlineDev } from '../cli'
 
 const program = new Command()
 
---
+++ src/cli/index.ts
@@ -0,0 +1,1 @@
+export { default as EndlineDev } from './endline-dev'
