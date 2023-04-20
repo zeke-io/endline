75dc0a3c06804b5eead175d4a1d37eb57b9f7dc9
zeke-io <ezequiel_io@pm.me>
2023-04-19 21:48:06 -0300

    Update "custom paths" example

--- examples/custom-paths/src/api/index.js
+++ examples/custom-paths/src/api/index.js
@@ -1,12 +1,11 @@
 async function IndexRoute(router) {
   router.GET('/', async ({ res }) => {
+    const payload = {
+      message: 'Hello from "src/api".',
+    }
+
     res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(
-      JSON.stringify({
-        message:
-          'This route is found in the "src/api" directory instead of the default "src/routes".',
-      }),
-    )
+    res.end(JSON.stringify(payload))
   })
 }
 
