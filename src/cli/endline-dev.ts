230a5d127f2c98d80b387fabf7f1de372f34e3e2
zeke-io <ezequiel_io@pm.me>
2023-04-20 15:30:54 -0300

    Add warning to endline dev command

--- src/cli/endline-dev.ts
+++ src/cli/endline-dev.ts
@@ -2,6 +2,7 @@ import { Command, Option } from 'commander'
 import { getProjectDirectory } from '../lib/directory-resolver'
 import { createServer } from '../server/http-server'
 import loadConfig from '../server/config'
+import { warn } from '../lib/logger'
 
 const command = new Command('dev')
   .description('Starts the Endline App in development mode')
@@ -15,6 +16,10 @@ const command = new Command('dev')
   .option('-d, --directory <path>', 'set the root directory of the project')
   .action(run)
 
+function sleep() {
+  return new Promise((resolve) => setTimeout(resolve, 4000))
+}
+
 async function run(options: any) {
   // eslint-disable-next-line prefer-const
   let { port, hostname, environment, directory } = options
@@ -24,6 +29,11 @@ async function run(options: any) {
   if (!hostname) hostname = 'localhost'
   if (isNaN(port)) port = 3000
   if (!environment) environment = 'development'
+
+  warn(
+    'This project is currently under development, and it is not ready for production.',
+  )
+  await sleep()
 
   const config = await loadConfig({ projectDir, environment })
   await createServer({ port, hostname, projectDir, config })
