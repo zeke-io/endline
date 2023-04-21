d8ea69ce1cd26d898717cd40e2ca1014329f7ecc
zeke-io <ezequiel_io@pm.me>
2023-04-21 14:06:39 -0300

    Add server to main server script

--- src/server/build/index.ts
+++ src/server/build/index.ts
@@ -4,9 +4,33 @@ import fs from 'fs'
 import path from 'path'
 import { findRouters } from '../router/router-loader'
 
-const mainServerContents = `const port = parseInt(process.env.PORT, 10) || 3000
+const mainServerContents = `const http = require('http')
+const { EndlineServer } = require('endline/dist/server/endline')
 
-console.log(\`Server is ready and listening on port \${port}\`)`
+const port = parseInt(process.env.PORT, 10) || 3000
+const server = http.createServer(async (req, res) => {
+  await requestListener(req, res)
+})
+
+let requestListener
+
+server.listen(port, async (err) => {
+  if (err) {
+    console.error(\`An error has occurred starting the server!\`, err)
+    process.exit(1)
+  }
+
+  const endline = new EndlineServer({
+    httpServer: server,
+    projectDir: __dirname,
+    config: { router: { routesDirectory: 'routes/' } }
+  })
+  requestListener = endline.requestListener
+  await endline.initialize()
+
+  console.log(\`Server is ready and listening on port "\${port}"\`)
+})
+`
 
 export default async function build({
   projectDir,
