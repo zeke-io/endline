92c9a17334a9263549379558fc35f647b4215a65
zeke-io <ezequiel_io@pm.me>
2023-04-21 23:12:26 -0300

    Add main server file

--- src/server/build/index.ts
+++ src/server/build/index.ts
@@ -4,34 +4,6 @@ import fs from 'fs'
 import path from 'path'
 import { findRouters } from '../router/router-loader'
 
-const mainServerContents = `const http = require('http')
-const { EndlineServer } = require('endline/dist/server/endline')
-
-const port = parseInt(process.env.PORT, 10) || 3000
-const server = http.createServer(async (req, res) => {
-  await requestListener(req, res)
-})
-
-let requestListener
-
-server.listen(port, async (err) => {
-  if (err) {
-    console.error(\`An error has occurred starting the server!\`, err)
-    process.exit(1)
-  }
-
-  const endline = new EndlineServer({
-    httpServer: server,
-    projectDir: __dirname,
-    config: { router: { routesDirectory: 'routes/' } }
-  })
-  requestListener = endline.requestListener
-  await endline.initialize()
-
-  console.log(\`Server is ready and listening on port "\${port}"\`)
-})
-`
-
 export default async function build({
   projectDir,
   config,
@@ -54,8 +26,11 @@ export default async function build({
 
   await compiler.run(outputPath)
 
-  await fs.promises.writeFile(
-    path.join(outputPath, 'main.js'),
-    mainServerContents,
+  /**
+   * Write the main.js server file to run the built app
+   */
+  const mainServerFile = await fs.promises.readFile(
+    path.join(__dirname, './_main.js'),
   )
+  await fs.promises.writeFile(path.join(outputPath, 'main.js'), mainServerFile)
 }
