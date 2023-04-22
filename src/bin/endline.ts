c2ebeab6d48754c52036a759a611a11deb8cc028
zeke-io <ezequiel_io@pm.me>
2023-04-22 00:46:55 -0300

    Move alpha warning to binary

--- src/bin/endline.ts
+++ src/bin/endline.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
 import { EndlineDev, EndlineBuild } from '../cli'
+import { warn } from '../lib/logger'
 
 const program = new Command()
 
@@ -8,4 +9,8 @@ program.name('endline')
 program.addCommand(EndlineDev.command, { isDefault: true })
 program.addCommand(EndlineBuild.command)
 
+warn(
+  `This project is still in its early stages and under active development.`,
+  `\nIf you'd like to contribute with code, reporting issues or giving suggestions, check out the project's repository: https://github.com/zeke-io/endline.`,
+)
 program.parse()
--- src/cli/endline-dev.ts
+++ src/cli/endline-dev.ts
@@ -2,7 +2,6 @@ import { Command, Option } from 'commander'
 import { getProjectDirectory } from '../lib/directory-resolver'
 import { initializeApp } from '../server'
 import loadConfig from '../server/config'
-import { warn } from '../lib/logger'
 
 const command = new Command('dev')
   .description('Starts the Endline App in development mode')
@@ -16,25 +15,15 @@ const command = new Command('dev')
   .option('-d, --directory <path>', 'set the root directory of the project')
   .action(run)
 
-function sleep() {
-  return new Promise((resolve) => setTimeout(resolve, 4000))
-}
-
 async function run(options: any) {
   // eslint-disable-next-line prefer-const
   let { port, hostname, environment, directory } = options
-
-  const projectDir = getProjectDirectory(directory)
 
   if (!hostname) hostname = 'localhost'
   if (isNaN(port)) port = 3000
   if (!environment) environment = 'development'
 
-  warn(
-    'This project is currently under development, and it is not ready for production.',
-  )
-  await sleep()
-
+  const projectDir = getProjectDirectory(directory)
   const config = await loadConfig({ projectDir, environment })
   await initializeApp({ port, hostname, projectDir, config })
 }
