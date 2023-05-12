23d7ad11894f676c60436f5d045a6c00cd3c6df9
zeke-io <ezequiel_io@pm.me>
2023-05-12 19:33:48 -0300

    Add prompts to create-endline-app

--- packages/create-endline-app/src/index.ts
+++ packages/create-endline-app/src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
+import prompts from 'prompts'
 
 const program = new Command()
 
@@ -8,5 +9,23 @@ program
   .version('0.0.1')
   .description('Create an Endline App')
   .argument('[name]', 'name of the project')
+  .action(main)
 
 program.parse()
+
+async function main(name: string) {
+  /** Validate arguments provided, prompt required options */
+  if (!name) {
+    const response = await prompts({
+      name: 'name',
+      type: 'text',
+      message: 'Name of the project',
+      initial: 'endline-api',
+      validate: (value) => (value ? true : 'Invalid project name'),
+    })
+
+    if (response.name) {
+      name = response.name.trim()
+    }
+  }
+}
