e94c34800d23692c285886b23a0344933bb107e7
zeke-io <ezequiel_io@pm.me>
2023-05-20 15:22:46 -0300

    Update route node

--- packages/endline/src/server/router/route-node.ts
+++ packages/endline/src/server/router/route-node.ts
@@ -1,35 +1,76 @@
 import { HTTPMethod } from '../http'
-import { RouteHandler } from './handler-types'
+import { HandlerContext, RouteHandler } from './handler-types'
 
 type Handlers = { [method in HTTPMethod]?: RouteHandler }
 
 export class RouteNode {
+  private path: string
+  private methods: Handlers
+
+  /** @deprecated this will be removed after the new routing implementation */
   public name: string
+  /** @deprecated this will be removed after the new routing implementation */
   public isParam: boolean
-  public methods: Handlers
-  /**
-   * @deprecated this will be removed after the new routing implementation
-   */
+  /** @deprecated this will be removed after the new routing implementation */
   public children: Map<string, RouteNode>
 
-  constructor(name?: string, handlers?: Handlers) {
-    this.name = name || ''
-    this.isParam = this.name.startsWith(':')
-    this.children = new Map()
+  // TODO: Make path not optional
+  constructor(path?: string, handlers?: Handlers) {
+    this.path = path || '/'
     this.methods = handlers || {}
+
+    this.name = path || ''
+    this.isParam = this.name.startsWith(':')
+
+    this.children = new Map()
   }
 
-  public getHandler(method: HTTPMethod): RouteHandler | undefined {
-    return this.methods[method]
-  }
-
+  /**
+   * @param method The request method
+   * @param handler The handler to save with the given method
+   * @return {boolean} It will return true if the method was not registered already, false if otherwise
+   */
   public addHandler(method: HTTPMethod, handler: RouteHandler): boolean {
-    // Return false if the method is already registered
-    if (this.getHandler(method) != null) {
+    if (this.hasMethod(method)) {
       return false
     }
 
     this.methods[method] = handler
     return true
+  }
+
+  /**
+   * Checks if there is a handler registered with the given method
+   *
+   * @param {HTTPMethod} method The request method
+   */
+  public hasMethod(method: HTTPMethod) {
+    return !!this.methods[method]
+  }
+
+  /**
+   * Gets the route handler registered with the given method
+   *
+   * @param method The request method
+   * @return route handler, undefined if not found
+   */
+  public getHandler(method: HTTPMethod): RouteHandler | undefined {
+    return this.methods[method]
+  }
+
+  /**
+   * Runs the route handler with the given method and context
+   *
+   * @private
+   * @param {HTTPMethod} method The request method
+   * @param {HandlerContext} context The context of the request
+   */
+  public async run(method: HTTPMethod, context: HandlerContext) {
+    const handler = this.getHandler(method)
+
+    // TODO: Handle exceptions
+    if (handler) {
+      await handler(context)
+    }
   }
 }
