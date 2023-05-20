350384c16bb869e063265159bd6afcaa94f1734f
zeke-io <ezequiel_io@pm.me>
2023-05-20 15:01:17 -0300

    Update layer

--- packages/endline/src/server/router/layer.ts
+++ packages/endline/src/server/router/layer.ts
@@ -1,1 +1,29 @@
-export class Layer {}
+import {
+  ParseOptions,
+  pathToRegexp,
+  TokensToRegexpOptions,
+} from 'path-to-regexp'
+import { HandlerContext, RouteHandler } from './handler-types'
+
+export class Layer {
+  private pathRegex: RegExp
+  private readonly handler: RouteHandler
+
+  constructor(path: string, handler: RouteHandler) {
+    const options: TokensToRegexpOptions & ParseOptions = {
+      sensitive: false,
+    }
+    this.pathRegex = pathToRegexp(path, [], options)
+    this.handler = handler
+  }
+
+  public match(path: string): boolean {
+    const foundMatch = this.pathRegex.exec(path)
+
+    return !!foundMatch
+  }
+
+  public handleRequest(context: HandlerContext) {
+    this.handler(context)
+  }
+}
