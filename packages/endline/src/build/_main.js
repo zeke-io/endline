309c6b958f69b58d3f33769f64bd0f48eabcac5a
zeke-io <ezequiel_io@pm.me>
2023-06-01 20:45:29 -0300

    Handle termination signals in _main.js

--- packages/endline/src/build/_main.js
+++ packages/endline/src/build/_main.js
@@ -1,7 +1,13 @@
 const http = require('http')
 const endline = require('endline/dist/endline')
-const hostname = 'localhost'
+
+process.env.NODE_ENV = 'production'
+process.on('SIGTERM', () => process.exit(0))
+process.on('SIGINT', () => process.exit(0))
+
+const hostname = process.env.HOSTNAME || 'localhost'
 const port = parseInt(process.env.PORT, 10) || 3000
+
 const config = {
   distDir: '.',
 }
