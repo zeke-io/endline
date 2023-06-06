959165e318c3175a2cbf68de922e190547f2de60
zeke-io <ezequiel_io@pm.me>
2023-06-06 14:27:03 -0300

    Update 'with-env-files' example

--- examples/with-env-files/src/routes/index.js
+++ examples/with-env-files/src/routes/index.js
@@ -1,14 +1,12 @@
 const GITHUB_REPOSITORY_URL = process.env.GITHUB_REPOSITORY_URL
 const NPM_PACKAGE_URL = process.env.NPM_PACKAGE_URL
 
+/** @param {import('endline').Router} router */
 export default async function IndexRoute(router) {
-  router.GET('/', async ({ res }) => {
-    const payload = {
+  router.GET('/', () => {
+    return {
       repositoryUrl: GITHUB_REPOSITORY_URL,
       npmPackageUrl: NPM_PACKAGE_URL,
     }
-
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(payload))
   })
 }
